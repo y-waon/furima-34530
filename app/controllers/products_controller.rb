3f5abd9f3900892eadf83dfe6b70c287cf5c7f6a
y-waon <you0923fs@gmail.com>
2021-02-23 16:43:44 +0900

    商品一覧表示機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
+    @products = Product.all
   end
   def new
     @product = Product.new
