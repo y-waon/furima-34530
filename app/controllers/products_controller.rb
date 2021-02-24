380ef91d8c7c96a71b11d294d3e9e89b92f2a2c5
y-waon <you0923fs@gmail.com>
2021-02-24 12:15:44 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,17 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
   private
 
   def product_params
